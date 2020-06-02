ca9ef8c2fe402b98668cfcc8b1643b524c275ac2
Mario <mario.medrano.maldonado@gmail.com>
2020-06-02 02:13:21 -0400

    Comment script steps

--- package-generator/pack.sh
+++ package-generator/pack.sh
@@ -1,5 +1,13 @@
-#!/bin/bash -ex
-cd package-generator
-tsc
-mv lib/* ../src/package/dist
-rm -rf lib
+#!/bin/bash
+echo "1. Go to folder where tsconfig.ts lays"
+    cd package-generator
+echo "2. Transpile Typescript to Javascript"
+    # "tsc" command uses tsconfig.ts as a 
+    # set of instructions of how to transpile
+    tsc
+    # A "lib" folder with the transpiled files was generated
+echo "3. Move all transpiled files to its final destination"
+    mv lib/* ../src/package/dist
+echo "4. Remove no longer needed files/folders"
+    rm -rf lib
+echo "5. Process complete!"
